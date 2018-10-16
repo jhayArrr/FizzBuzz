05a93838313fef179cc4cadc6a0c93b9c1d8c7dd
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-16 12:15:41 -0600

    changes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const testNumber = 15;
 console.log(fizzBuzz(testNumber)); //Calling Function
 
 function fizzBuzz (input) { //These match order of logic
-  if (input % 3 === 0) return "Fizz";
-  if (input % 5 === 0) return 'Buzz';
   if ((input % 3 === 0 ) && (input % 5 === 0)) return 'FizzBuss';
+  if (input % 3 === 0 ) return "Fizz";
+  if (input % 5 === 0) return 'Buzz';
   if (typeof input !=='number') return 'This is not a number.'; //Change to NaN
   return "This test number does nothing important. The input was " + input + ".";
 }
